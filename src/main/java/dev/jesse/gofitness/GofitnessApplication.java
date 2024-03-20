dd1b116c073db36dceb458bd2ace88a854b0ccb0
Manekineko-mk1 <HoHoMrChan@gmail.com>
2024-03-20 13:29:13 -0400

    first commit

---
+++ src/main/java/dev/jesse/gofitness/GofitnessApplication.java
@@ -0,0 +1,13 @@
+package dev.jesse.gofitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofitnessApplication.class, args);
+	}
+
+}
